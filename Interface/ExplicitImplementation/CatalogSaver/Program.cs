8a6c745b3000e874b2cba967d14fdec0007cddc4
ousmane <ousmane4@live.fr>
2022-12-21 09:16:11 +0100

    explicit interface IEnumerable of <T>

---
+++ Interface/ExplicitImplementation/CatalogSaver/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CatalogSaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ISaveable saveable = new Catalog();
+            //saveable.Save();
+
+            Catalog catalog = new Catalog();
+            ((ISaveable)catalog).Save();
+        }
+    }
+}
