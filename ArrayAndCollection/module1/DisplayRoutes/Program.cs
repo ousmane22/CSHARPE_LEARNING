6ff6ed28a7582f4ba725742e2a0f9ff38fecd214
ousmane <ousmane4@live.fr>
2022-12-29 13:48:14 +0100

    array and collection intro

---
+++ ArrayAndCollection/module1/DisplayRoutes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pluralsight.ArraysCollections.Demos
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BusRoute route40 = new BusRoute(40, "Morecambe", "Preston");
+			BusRoute route42 = new BusRoute(42, "Lancaster", "Blackpool");
+
+			BusRoute[] routes =
+			{
+				route40,
+				route42,
+				new BusRoute(100, "University", "Morecambe"),
+				new BusRoute(555, "Lancaster", "Keswick")
+			};
+
+		
+
+			foreach (BusRoute route in routes)
+				Console.WriteLine(route);
+		}
+	}
+}
