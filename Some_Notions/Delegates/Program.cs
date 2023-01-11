814a2a605252fb507188c2adea0ba85bdba3dfbb
ousmane <ousmane4@live.fr>
2023-01-11 16:55:51 +0100

    delagates

---
+++ Some_Notions/Delegates/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+public delegate void HelloFonctionDelegate(string message);
+
+public class program
+{
+    public static void Main()
+    {
+        HelloFonctionDelegate handle = Hello;
+        handle("Hello from delage");
+    }
+    public static void Hello(string msgHello)
+    {
+        Console.WriteLine(msgHello);
+    }
+}
