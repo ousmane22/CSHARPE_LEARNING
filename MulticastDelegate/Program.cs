63f42b141d8919503682aeeb2160607f8b16569c
ousmane <ousmane4@live.fr>
2023-01-12 00:29:07 +0100

    Multicast Delegates

---
+++ MulticastDelegate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MulticastDelegate
+{
+    delegate void CustomDel(string s);
+    public  class Program
+    {
+
+        public static void Hello(string s)
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public static void GoodBye(string s)
+        {
+            Console.WriteLine("Good Bye");
+        }
+        public  static void Main()
+        {
+            CustomDel hiDel, byeDel, multiDel, MultiMinusHidel;
+
+            hiDel = Hello;
+            byeDel = GoodBye;
+
+            multiDel = hiDel + byeDel;
+
+            MultiMinusHidel = multiDel - hiDel;
+
+
+            Console.WriteLine("Invoking delegate hiDel:");
+            hiDel("A");
+            Console.WriteLine("Invoking delegate byeDel:");
+            byeDel("B");
+            Console.WriteLine("Invoking delegate multiDel:");
+            multiDel("C");
+            Console.WriteLine("Invoking delegate multiMinusHiDel:");
+            MultiMinusHidel("D");
+        }
+
+
+
+       
+    }
+
+}
