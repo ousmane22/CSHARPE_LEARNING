a96c1e5e705afcf0e9b4bd056c03731501901ff0
ousmane <ousmane4@live.fr>
2023-04-14 12:20:49 +0100

    adding EF CORE to our app

---
+++ ASP DOT NET/NdiayeShop/Models/NdiayeShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NdiayeShop.Models
+{
+    public class NdiayeShopDbContext:DbContext
+    {
+        public NdiayeShopDbContext(DbContextOptions<NdiayeShopDbContext> 
+            options):base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pie> Pies { get; set; }
+    }
+}
--- ASP DOT NET/NdiayeShop/Program.cs
+++ ASP DOT NET/NdiayeShop/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using NdiayeShop.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<ICategoryRepository,MockCategoryRepository>();
 builder.Services.AddScoped<IPieRepository,MockPieRepository>();
+
+builder.Services.AddDbContext<NdiayeShopDbContext>(options => {
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:NdiayeShopConnection"]);
+});
 
 var app = builder.Build();
 
